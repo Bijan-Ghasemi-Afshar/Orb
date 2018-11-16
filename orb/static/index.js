458ad75d174affbe5f3fa84ca3f185e06b1a3119
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2018-11-16 18:02:13 +0000

    Basic website setup

---
+++ orb/static/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './home.css';
+
+console.log("Hello Dude");
---
+++ orb/static/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'home.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};

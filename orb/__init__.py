89c0bbd0cd84405a41f07b6e9be1afba82b13a75
Bijan Ghasemi Afshar <bijan.ghasemiafshar@gmail.com>
2018-11-20 15:19:34 +0000

    Added PyMongo to access mongodb database

--- orb/__init__.py
+++ orb/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import (Flask, render_template)
+from flask_pymongo import PyMongo
 
 
 def create_app(test_config=None):
@@ -8,8 +9,10 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        MONGO_URI="mongodb://localhost:27017/orbDatabase"
+        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    mongo=PyMongo(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -27,6 +30,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/')
     def hello():
-        return render_template('home.html')
+        test_data = mongo.db.testDoc.find_one_or_404()
+        return render_template('home.html', test_data=test_data)
 
     return app

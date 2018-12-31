59ee6ab62da44395cb18747cc97bca31528b3379
Richard <richysab@sky.com>
2018-12-31 15:54:57 +0000

    Added parse user input

--- orb/KB/chat_state_classifier.py
+++ orb/KB/chat_state_classifier.py
@@ -1,16 +1,12 @@
 from orb.RE import general_engine, ticket_engine
+from orb.KB import parse_input as parse_user_input
 
 # Classifies the chat state based on user input and returns the right engine
 def classify_chat(user_input):
+	text = parse_user_input.ParseText()
+	conversation = user_input
+	thisConversation=text.userInput(conversation)
+	print("Printing the token list:" + str(thisConversation))
+	print(thisConversation)
 
-	# book_ticket = True
-
-	# if book_ticket:            
-	# 	station = ticket_booking.get_station_abvr(user_input, mongo)
-	# if station is not None:            
-	# 	print(ticket_booking.construct_url(station["abbreviation"], "LST", "181218", "1600"))
-
-	# else:
-	# 	pass
-
-	return ticket_engine
+	return general_engine
---
+++ orb/KB/parse_input.py
@@ -0,0 +1,145 @@
+
+import nltk
+import csv
+import collections
+# import stopwords, tokens, stemmer, spelling
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from autocorrect import spell
+
+'''
+Text preprocesing class to parse user text. All text is processed in the following order: reduced to lower case, punctation removed,
+tokenised, stopwords removed, stem words and OPTIONAL: change supported language (english default), spell check text.
+
+'''
+class ParseText:
+
+    # set the language of our input text add functionality to control later
+    language = 'english'
+    spelling = True
+
+    def __init__(self):
+        self.inputText = []
+
+    '''
+    Function for testing text
+    '''
+    def testing(self):
+
+        print("Testing packages support")
+        # Print out the supported stopwords languages
+        print(stopwords.fileids())
+        # Print out english stopwords 
+        print (stopwords.words('english'))
+
+    '''
+    Set the system text input langauge 
+    '''
+    def setLanguage(self):
+
+        language = 'english'
+        valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+        print("The default language support is: "+language)
+        print("Would you like to change the language? Supported langauges are: "+ str(stopwords.fileids()))
+        answer = input("Change langauge: [y/n]")
+        if answer in (valid):
+            #answer = input("Change langauge: [y/n]")          
+            if answer == "yes":
+                print("Please enter new Language:")
+                langauge =input()
+                print(langauge)
+            elif answer == "no":
+                print("langauage is:"+ langauge)
+            else:
+                print("Please enter yes or no.")
+
+
+    '''
+    Function to activate or deactivate the spell checker
+    '''
+    def setSpellChecker(self):
+
+        global spelling 
+        valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+        print("The default spellcheck support is: "+ParseText.spelling)
+        print("Would you like to change the spellcheck?")
+        answer = input("Change spellcheck: [y/n]")
+        if answer in (valid):
+            
+            if answer == "yes":
+                print("Spell check active:")
+                spelling = True
+                print(ParseText.spelling)
+            elif answer == "no":
+                print("Spell check not active")
+                spelling =False
+                print(spelling)
+            else:
+                print("Please enter yes or no.")
+   
+    '''
+    Parse all the input conversation text
+    '''
+    def userInput(self, userText):
+
+        punctuations = '''!()-[]{};'"\,<>.?@#$%^&*_~'''
+        stemmer = PorterStemmer()
+
+        print ("processing user input")
+        # reduce uppercase text to lower case
+        userText = userText.lower()
+
+        # remove all punctuation
+        noPunct = ""
+        for char in userText:
+            if char not in punctuations:
+                noPunct = noPunct + char
+
+        # print(noPunct)
+        # split the sentence into tokens
+        conversation = nltk.word_tokenize(noPunct)
+        print("Printing the token list: " + str(conversation))
+
+        # remove stopwords
+        for word in conversation:
+            if word in stopwords.words(ParseText.language):
+                conversation.remove(word)
+        # print("Printing the token list: " + str(conversation)) 
+
+        # stem the words 
+        for i in range(len(conversation)):
+            conversation[i]= stemmer.stem(conversation[i])
+        # print("Printing the token list: " + str(conversation))
+
+        # spell check the text
+        if ParseText.spelling == True:
+            #print("spell test")
+            for j in range(len(conversation)):
+                conversation[j]= stemmer.stem(spell(conversation[j]))
+
+            print("Printing the token list: " + str(conversation))
+        return conversation
+
+
+
+    def testingLogic(self):
+        print("Logic testing")
+
+
+
+def main():
+    conversation = input("Hello, how can I help you?")
+    thisConversation=text.userInput(conversation)
+    print("Printing the token list:" + str(thisConversation))
+
+if __name__ == "__main__":
+
+    text = ParseText()
+    main()
+    #text.testing()
+    #text.setLanguage()
+    
+    
